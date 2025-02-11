d368b6926af4016ad6a302d9dd78fa3ac5a313bb
REWELL Munene <riwenamu@gmail.com>
2025-02-11 17:35:51 +0300

    Add files via upload

---
+++ Standard.Calculator/app.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
+
+function showTime() {
+    const now = new Date();
+    alert(`Current Time: ${now.toLocaleTimeString()}`);
+}
