89349047073d5535f5dd0f624eab95b82c720218
rewell namu <riwenamu@gmail.com>
2025-03-20 19:10:33 +0300

    Refactor app component structure and styles; replace placeholder content with HomeComponent and update CSS for better styling

--- ANGULAR/angularapp/src/app/app.component.ts
+++ ANGULAR/angularapp/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angularapp';
--- ANGULAR/angularapp/src/app/home/home.component.ts
+++ ANGULAR/angularapp/src/app/home/home.component.ts
@@ -6,21 +6,5 @@
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  newTask: string = '';
-  tasks: { name: string; completed: boolean }[] = [];
-
-  addTask() {
-    if (this.newTask.trim()) {
-      this.tasks.push({ name: this.newTask, completed: false });
-      this.newTask = '';
-    }
-  }
-
-  toggleTask(index: number) {
-    this.tasks[index].completed = !this.tasks[index].completed;
-  }
-
-  deleteTask(index: number) {
-    this.tasks.splice(index, 1);
-  }
+  message: string = 'Hello, Angular!'
 }
